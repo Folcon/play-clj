43eef569c06d97c950734702120659d11ba1e8ef
Folcon <folcon@gmail.com>
2021-08-07 23:03:10 +0100

    Updated to work with libgdx "1.10.0"

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[com.badlogicgames.gdx/gdx "1.9.3"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.9.3"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.9.3"]
-                 [org.clojure/clojure "1.7.0"]]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.10.0"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.10.0"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.10.0"]
+                 [org.clojure/clojure "1.10.2-alpha2"]]
   :repositories [["sonatype"
                   "https://oss.sonatype.org/content/repositories/releases/"]]
   :source-paths ["src"]
   :java-source-paths ["src-java"]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"])
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"])
